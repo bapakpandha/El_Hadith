3a300c94e255094953bd9a28acef4bea3cfb4602
walikota wuhan <shopieorgil30@gmail.com>
2023-10-22 00:01:08 +0700

    Merapikan segalanya

---
+++ GlobalAssets/js/load_vendor.js
@@ -0,0 +1,6 @@
+var vendors_in_head = '<script src="../GlobalAssets/vendor/popper/popper.min.js"></script>' +
+    '<link rel="stylesheet" href="../GlobalAssets/vendor/bootstrap/bootstrap.min.css">' +
+    '<script src="../GlobalAssets/vendor/bootstrap/bootstrap.min.js"></script>';
+
+
+$('head').append(vendors_in_head);
